22c0b3c4aaa24afce1a9f0991cdf96cc6b005eda
Gaspar Tessi <agaspartessi@gmail.com>
2022-08-30 14:02:12 -0300

    Message in greetings

--- index.php
+++ index.php
@@ -22,6 +22,7 @@
 
 require_once('../../config.php');
 require_once($CFG->dirroot. '/local/greetings/lib.php');
+require_once($CFG->dirroot. '/local/greetings/message_form.php');
 
 $context = context_system::instance();
 $PAGE->set_context($context);
@@ -33,11 +34,20 @@ $PAGE->set_title($SITE->fullname);
 $PAGE->set_heading(get_string('pluginname', 'local_greetings'));
 
 echo $OUTPUT->header();
+$messageform = new local_greetings_message_form();
 
 if (isloggedin()) {
     echo local_greetings_get_greeting($USER);
 } else {
     echo get_string('greetinguser', 'local_greetings');
+}
+
+$messageform->display();
+
+if ($data = $messageform->get_data()) {
+    $message = required_param('message', PARAM_TEXT);
+
+    echo $OUTPUT->heading($message, 4);
 }
 
 echo $OUTPUT->footer();
